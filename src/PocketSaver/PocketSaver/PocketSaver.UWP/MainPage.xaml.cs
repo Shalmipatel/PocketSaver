3afb26556db9248e1546001a932d54d4183342b3
Mevin Mathew <mevin.mathew@hotmail.com>
2017-09-25 18:24:47 -0400

    Created the initial project.

---
+++ src/PocketSaver/PocketSaver/PocketSaver.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PocketSaver.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new PocketSaver.App());
+        }
+    }
+}

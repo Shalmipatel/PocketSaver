05c1fe420355d4fb50865bbac2f31f44daa1eb16
Mevin Mathew <mevin.mathew@hotmail.com>
2017-09-25 12:20:29 -0400

    Created the initial master detail page and slideout menu as well as tinted image renderers.

---
+++ PocketSaver/PocketSaver/Views/MainMasterDetailPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PocketSaver.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PocketSaver.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMasterDetailPage : MasterDetailPage
+    {
+        /// <summary>
+        /// Constructor for the MainMasterDetailPage.
+        /// </summary>
+		public MainMasterDetailPage()
+        {
+            InitializeComponent();
+
+            MainMenuPage.ListView.ItemSelected += OnItemSelected;
+        }
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                MainMenuPage.ListView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+
+        public void NavigateTo(Page page)
+        {
+            Detail = new NavigationPage(page);
+        }
+    }
+}

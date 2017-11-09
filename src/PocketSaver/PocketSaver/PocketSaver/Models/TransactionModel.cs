f8bd27f4986ebe52dee45b61ec43a043631b36a3
Mevin Mathew <mevin.mathew@hotmail.com>
2017-11-09 18:32:45 -0500

    Created and completed the ApiSV module

---
+++ src/PocketSaver/PocketSaver/PocketSaver/Models/TransactionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketSaver.Models
+{
+    public class TransactionModel
+    {
+        public string _id { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public decimal PurchaseAmount { get; set; }
+    }
+}

4d5b5590dd4a311747f0ed2ba1131c154951e80a
Mevin Mathew <mevin.mathew@hotmail.com>
2017-11-22 02:39:34 -0500

    Created new documentation for the classes that I have worked on.

--- Doc/Design/MIS/html/search/classes_b.js
+++ Doc/Design/MIS/html/search/classes_b.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['tintedimage',['TintedImage',['../class_pocket_saver_1_1_views_1_1_shared_1_1_tinted_image.html',1,'PocketSaver::Views::Shared']]],
+  ['transactiondetail',['TransactionDetail',['../class_pocket_saver_1_1_views_1_1_transaction_1_1_transaction_detail.html',1,'PocketSaver::Views::Transaction']]],
+  ['transactioneditpage',['TransactionEditPage',['../class_pocket_saver_1_1_views_1_1_transaction_1_1_transaction_edit_page.html',1,'PocketSaver::Views::Transaction']]],
   ['transactionlistcell',['TransactionListCell',['../class_pocket_saver_1_1_views_1_1_transaction_1_1_transaction_list_cell.html',1,'PocketSaver::Views::Transaction']]],
   ['transactionlistpage',['TransactionListPage',['../class_pocket_saver_1_1_views_1_1_transaction_1_1_transaction_list_page.html',1,'PocketSaver::Views::Transaction']]],
   ['transactionmodel',['TransactionModel',['../class_pocket_saver_1_1_models_1_1_transaction_model.html',1,'PocketSaver::Models']]],
---
+++ Doc/Design/MIS/html/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['navigateto',['NavigateTo',['../class_pocket_saver_1_1_views_1_1_main_master_detail_page.html#a5350164e6e86879fc93ba9821459eccf',1,'PocketSaver::Views::MainMasterDetailPage']]]
+];

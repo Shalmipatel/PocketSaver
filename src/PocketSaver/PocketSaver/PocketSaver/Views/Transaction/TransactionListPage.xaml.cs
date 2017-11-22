e43bde02c13460eb6adf628116f4293eb6638b6b
Mevin Mathew <mevin.mathew@hotmail.com>
2017-11-22 02:28:43 -0500

    Commented all pages that I have worked on thus far.

--- src/PocketSaver/PocketSaver/PocketSaver/Views/Transaction/TransactionListPage.xaml.cs
+++ src/PocketSaver/PocketSaver/PocketSaver/Views/Transaction/TransactionListPage.xaml.cs
@@ -12,10 +12,20 @@ using Xamarin.Forms.Xaml;
 namespace PocketSaver.Views.Transaction
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    /// <summary>
+    /// Class for the TransactionListPage of the PocketSaver Mobile Application.
+    /// </summary>
     public partial class TransactionListPage : ContentPage
     {
+        /// <summary>
+        /// Declaring the TransactionViewModel object.
+        /// </summary>
         TransactionViewModel viewModel;
 
+        /// <summary>
+        /// Constructor for the TransactionListPage which initializes the TransactionViewModel and defines the listview item selection.
+        /// </summary>
         public TransactionListPage()
         {
             InitializeComponent();
@@ -34,7 +44,9 @@ namespace PocketSaver.Views.Transaction
 
             };
         }
-
+        /// <summary>
+        /// OnAppearing Method which is used to declare variables and methods to be called upon the page appearing.
+        /// </summary>
         protected override async void OnAppearing()
         {
             base.OnAppearing();
@@ -43,9 +55,14 @@ namespace PocketSaver.Views.Transaction
             transactionList.ItemsSource = TransactionViewModel.transactionDatum;
             load.IsRunning = false;
             BindingContext = viewModel;
-            
+
         }
 
+        /// <summary>
+        /// Method for the addButton when clicked which creates a new TransactionEditPage.
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">EventArgument</param>
         private void addButton_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new TransactionEditPage());

6e55ff94c079a022035f3cd8ccb44a3976eb1e42
Shalmi Patel <patels19@mcmaster.ca>
2017-11-22 22:18:31 -0500

    Updated HomePage View and created HomePage ViewModel

---
+++ src/PocketSaver/PocketSaver/PocketSaver/ViewModels/HomePage/HomePageViewModel.cs
@@ -0,0 +1,104 @@
+﻿using PocketSaver.ViewModels.Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketSaver.ViewModels.HomePage
+{
+    public class HomePageViewModel
+    {
+        public decimal janTot;
+        public decimal febTot;
+        public decimal marTot;
+        public decimal aprTot;
+        public decimal mayTot;
+        public decimal junTot;
+        public decimal julTot;
+        public decimal augTot;
+        public decimal sepTot;
+        public decimal octTot;
+        public decimal novTot;
+        public decimal decTot;
+        public decimal dailyTot;
+
+        public HomePageViewModel()
+        {
+            janTot = 0;
+            febTot = 0;
+            marTot = 0;
+            aprTot = 0;
+            mayTot = 0;
+            junTot = 0;
+            julTot = 0;
+            augTot = 0;
+            sepTot = 0;
+            octTot = 0;
+            novTot = 0;
+            decTot = 0;
+            dailyTot = 0;
+
+
+        }
+
+        public async void CalcMonth()
+        {
+            await TransactionViewModel.RefreshList();
+
+            foreach (var x in TransactionViewModel.transactionDatum)
+            {
+                switch (x.Date.Month){
+                    case 1:
+                        janTot += x.PurchaseAmount;
+                        break;
+                    case 2:
+                        febTot += x.PurchaseAmount;
+                        break;
+                    case 3:
+                        marTot += x.PurchaseAmount;
+                        break;
+                    case 4:
+                        aprTot += x.PurchaseAmount;
+                        break;
+                    case 5:
+                        mayTot += x.PurchaseAmount;
+                        break;
+                    case 6:
+                        junTot += x.PurchaseAmount;
+                        break;
+                    case 7:
+                        julTot += x.PurchaseAmount;
+                        break;
+                    case 8:
+                        augTot += x.PurchaseAmount;
+                        break;
+                    case 9:
+                        sepTot += x.PurchaseAmount;
+                        break;
+                    case 10:
+                        octTot += x.PurchaseAmount;
+                        break;
+                    case 11:
+                        novTot += x.PurchaseAmount;
+                        break;
+                    case 12:
+                        decTot += x.PurchaseAmount;
+                        break;
+                    default:
+                        break;
+                }
+
+                if(x.Date.Day == DateTime.Now.Day)
+                {
+                    dailyTot += x.PurchaseAmount;
+                }
+
+            }
+
+            
+
+        }
+
+    }
+}
--- src/PocketSaver/PocketSaver/PocketSaver/Views/Home/HomePage.xaml.cs
+++ src/PocketSaver/PocketSaver/PocketSaver/Views/Home/HomePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PocketSaver.ViewModels.HomePage;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,129 @@ using Xamarin.Forms.Xaml;
 
 namespace PocketSaver.Views.Home
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HomePage : ContentPage
-	{
-		public HomePage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        HomePageViewModel viewModel;
+        public static ObservableCollection<String> monthList;
+
+
+        public HomePage()
+        {
+            InitializeComponent();
+            //HomePageViewModel.CalcMonth()
+            viewModel = new HomePageViewModel();
+            viewModel.CalcMonth();
+            monthList = new ObservableCollection<string>();
+            monthList.Add("January");
+            monthList.Add("Febuary");
+            monthList.Add("March");
+            monthList.Add("April");
+            monthList.Add("May");
+            monthList.Add("June");
+            monthList.Add("July");
+            monthList.Add("August");
+            monthList.Add("September");
+            monthList.Add("October");
+            monthList.Add("November");
+            monthList.Add("December");
+            monthPicker.Title = "Select a Month";
+            monthPicker.ItemsSource = monthList;
+
+            monthPicker.SelectedIndexChanged += (sender, e) =>
+            {
+                String amount;
+                if (monthPicker.SelectedIndex != -1)
+                {
+                    switch (monthPicker.SelectedIndex)
+                    {
+                        case 0:
+                            amount = Convert.ToString(viewModel.janTot);
+                            Amount.Text = amount;
+                            break;
+                        case 1:
+                            amount = Convert.ToString(viewModel.febTot);
+                            Amount.Text = amount;
+                            break;
+                        case 2:
+                            amount = Convert.ToString(viewModel.marTot);
+                            Amount.Text = amount;
+                            break;
+                        case 3:
+                            amount = Convert.ToString(viewModel.aprTot);
+                            Amount.Text = amount;
+                            break;
+                        case 4:
+                            amount = Convert.ToString(viewModel.mayTot);
+                            Amount.Text = amount;
+                            break;
+                        case 5:
+                            amount = Convert.ToString(viewModel.junTot);
+                            Amount.Text = amount;
+                            break;
+                        case 6:
+                            amount = Convert.ToString(viewModel.julTot);
+                            Amount.Text = amount;
+                            break;
+                        case 7:
+                            amount = Convert.ToString(viewModel.augTot);
+                            Amount.Text = amount;
+                            break;
+                        case 8:
+                            amount = Convert.ToString(viewModel.sepTot);
+                            Amount.Text = amount;
+                            break;
+                        case 9:
+                            amount = Convert.ToString(viewModel.octTot);
+                            Amount.Text = amount;
+                            break;
+                        case 10:
+                            amount = Convert.ToString(viewModel.novTot);
+                            Amount.Text = amount;
+                            break;
+                        case 11:
+                            amount = Convert.ToString(viewModel.decTot);
+                            Amount.Text = amount;
+                            break;
+                        default:
+                            Amount.Text = "Sorry Invalid Month Entered";
+                            break;
+                    }
+                    //monthPicker.Text = (string)monthPicker.ItemsSource[selectedIndex];
+                }
+            };
+
+
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            //  await HomePageViewModel
+
+            var monkeyNameLabel = new Label();
+            monthPicker.SetBinding(Label.TextProperty, new Binding("SelectedItem", source: monthList));
+        }
+
+        //void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        //{
+           
+        //    int selectedIndex = monthPicker.SelectedIndex;
+
+        //    if (selectedIndex != -1)
+        //    {
+        //        switch (selectedIndex)
+        //        {
+        //            case 1: Amount.Text = "Test";
+        //                    break;
+        //            default: Amount.Text = "0.01";
+        //                break;
+        //        }
+        //        //monthPicker.Text = (string)monthPicker.ItemsSource[selectedIndex];
+        //    }
+
+        //}
+
+
+    }
 }
